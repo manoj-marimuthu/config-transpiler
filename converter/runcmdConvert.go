eca8d69017fba952c02a3a2b5097d9d6f1f2eea4
Manoj K M <manojkm24dev@gmail.com>
2026-08-25 13:27:18 +0000

    Add tests for runcmdConvert

--- converter/runcmdConvert.go
+++ converter/runcmdConvert.go
@@ -5,25 +5,22 @@ import (
 	"fmt"
 )
 
-func runCmdConverter(in config.CloudConfig, out *config.Butane){
+func runCmdConvert(in config.CloudConfig, out *config.Butane){
 	for i, command := range in.RunCmd{
 		butaneUnit := config.ButaneSystemdUnit{
 			Name: fmt.Sprintf("cloud-init-runcmd-%d.service",i),
 			Enabled: true,
 			Contents: fmt.Sprintf(`[Unit]
 Description=cloud-init runcmd unit
-
 [Service]
 Type=oneshot
 ExecStart=/bin/sh -c '%s'
-
 [Install]
-WantedBy=multi-user.target
-			`, command),
+WantedBy=multi-user.target`, command),
 		}
 		out.Systemd.Units = append(out.Systemd.Units, butaneUnit)
 	}
 }
 func init(){
-	register(runCmdConverter)
+	register(runCmdConvert)
 }
---
+++ converter/runcmdConvert_test.go
@@ -0,0 +1,59 @@
+package converter
+
+import (
+	"config-transpiler/config"
+	"reflect"
+	"testing"
+)
+
+
+func TestRunCmd(t* testing.T){
+	tests := []struct {
+		in config.CloudConfig
+		out config.Butane
+	}{
+		{
+			config.CloudConfig{
+				RunCmd: []string{
+					"echo hello",
+					"ls -l",
+				},
+			},
+			config.Butane{
+				Systemd: config.Systemd{
+						Units : []config.ButaneSystemdUnit{
+							{
+								Name: "cloud-init-runcmd-0.service",
+								Enabled: true,
+								Contents: `[Unit]
+Description=cloud-init runcmd unit
+[Service]
+Type=oneshot
+ExecStart=/bin/sh -c 'echo hello'
+[Install]
+WantedBy=multi-user.target`,
+							},	
+							{
+								Name: "cloud-init-runcmd-1.service",
+								Enabled: true,
+								Contents: `[Unit]
+Description=cloud-init runcmd unit
+[Service]
+Type=oneshot
+ExecStart=/bin/sh -c 'ls -l'
+[Install]
+WantedBy=multi-user.target`,
+							},
+					},
+				},
+			},
+		},
+	}
+	for i,test := range tests{
+		var out config.Butane
+		runCmdConvert(test.in, &out)
+		if !reflect.DeepEqual(out, test.out){
+			t.Errorf("test %d, Expected: %+v, Received: %+v \n \n",i, test.out, out)
+		}
+	}
+}
